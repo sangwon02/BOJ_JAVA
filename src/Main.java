963b12633a4abb76f25fb5efb7a02c4c18e36041
이상원 <sangwon020214@gmail.com>
2025-08-29 23:05:16 +0900

    Feat: Initial commit for BOJ solutions

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    // 자바 프로그램은 이 main 메서드에서 실행을 시작
+    public static void main(String[] args) {
+        // main 메서드 안에서 Hello, World! 출력
+        System.out.println("Hello, World!");
+    }
+}
