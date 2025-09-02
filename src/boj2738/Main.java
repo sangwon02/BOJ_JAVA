2520f62a0b6d25ce9d4f010e9f8c8f853aebc5d2
이상원 <sangwon020214@gmail.com>
2025-09-03 00:35:02 +0900

    Solved: BOJ 2738 행렬 덧셈

---
+++ src/boj2738/Main.java
@@ -0,0 +1,43 @@
+package boj2738;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr1 = new int[n][m];
+
+        for (int i = 0; i < n; i++){
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = Integer.parseInt(st1.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] += Integer.parseInt(st1.nextToken());
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        br.close();
+
+    }
+
+}
