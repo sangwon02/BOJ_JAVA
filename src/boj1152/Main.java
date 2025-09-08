e99ccdaa6b155a5d6db11e42c3d520110f508a4b
이상원 <sangwon020214@gmail.com>
2025-09-08 22:01:08 +0900

    Solved: BOJ 1152 단어의 개수

---
+++ src/boj1152/Main.java
@@ -0,0 +1,18 @@
+package boj1152;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine().trim();
+
+        StringTokenizer st = new StringTokenizer(line);
+        System.out.println(st.countTokens());
+
+        br.close();
+    }
+}
