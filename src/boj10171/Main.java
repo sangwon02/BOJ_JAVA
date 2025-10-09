0d569cc41bd3f3d1406d9d9fe3af29fac436d3b9
이상원 <sangwon020214@gmail.com>
2025-10-09 23:26:04 +0900

    Solved: BOJ 10171 고양이

---
+++ src/boj10171/Main.java
@@ -0,0 +1,12 @@
+package boj10171;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
