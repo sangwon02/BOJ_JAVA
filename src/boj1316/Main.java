8cbaa9e3bce1e49ada3c26c0e988964e3197d60f
이상원 <sangwon020214@gmail.com>
2025-10-21 22:32:55 +0900

    Solved: BOJ 1316 그룹 단어 체커

--- src/boj1316/Main.java
+++ src/boj1316/Main.java
@@ -1,45 +1,53 @@
 package boj1316;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws  Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int groupWordCount = 0;
 
-        int cnt  = 0; // 그룹단어를 카운트할 변수
-
-        for(int  i = 0; i  < n; i++){
-            if(checkfunc(br.readLine())){ //한 줄씩 문자열을 받아서 만약 유효성이 통과되면
-                cnt++;
+        // n개의 단어를 입력받아 하나씩 확인
+        for (int i = 0; i < n; i++) {
+            // check() 함수를 호출해서 그룹 단어이면 카운트를 1 늘림
+            if (check(br.readLine())) {
+                groupWordCount++;
             }
         }
-
-
-        System.out.println(cnt); // 단어 개수를 출력한다.
+        System.out.println(groupWordCount);
         br.close();
     }
 
-    static boolean checkfunc(String str){
-        boolean[] checkAlpha = new boolean[26]; // 입력받은 값이 영어임으로 영어의 개수만큼 배열 생성
-        int prev = -1; // 이전 문자의 인덱스값을 저장하는 변수
+    // 그룹 단어인지 판별하는 함수
+    public static boolean check(String word) {
+        // 알파벳 등장 기록을 저장할 boolean 배열 생성 (a-z: 26개)
+        boolean[] appeared = new boolean[26];
+        //바로 이전 문자를 저장할 변수
+        char prevChar = 0;
 
-        for(int i = 0; i < str.length(); i++){
-            int now = str.charAt(i); // 현재의 문자의 아스키 코드값을 저장
+        // 단어의 각 문자를 처음부터 끝까지 확인한다
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
 
-            if(prev != now){ //이 전 문자와 i번째 문자가 같지 않다면
-                if(checkAlpha[now-97] == false){
-                    checkAlpha[now-97] = true;
-                    prev = now;
-
-                }else{
-                    return false;
+            // 이전 문자와 현재 문자가 다를 경우
+            if (prevChar != currentChar) {
+                // 이 새로운 문자가 과거에 등장한 기록이 있는지 확인
+                if (appeared[currentChar - 'a']) {
+                    // 기록이 있다면, 연속이 깨진 것이므로 그룹 단어가 아님
+                    return false; // 즉시 함수 종료
                 }
-            }else{
-                continue;
+
+                // 과거 기록에 없다면, 이 문자가 등장했다고 기록
+                appeared[currentChar - 'a'] = true;
+                // 이전 문자(prevChar)를 현재 문자(currentChar)로 갱신한다.
+                prevChar = currentChar;
             }
         }
-        return true; // 그룹 문자인 경우
+
+        // for문이 중간에 false로 끝나지 않고 무사히 완료되면 그룹 단어
+        return true;
     }
 }
