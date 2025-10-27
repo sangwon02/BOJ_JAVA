8954709669c5f5ff6c57c484897e1659008d61e5
이상원 <sangwon020214@gmail.com>
2025-10-27 23:31:11 +0900

    Solved: BOJ 2557 Hello World

---
+++ src/boj2557/Main.java
@@ -0,0 +1,9 @@
+package boj2557;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
