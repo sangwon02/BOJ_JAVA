65c0253da813a27c4fb99d62f1e46d5ccd9b0152
이상원 <sangwon020214@gmail.com>
2025-11-12 23:41:07 +0900

    Solved: BOJ 10430 나머지

---
+++ src/boj10430/Main.java
@@ -0,0 +1,28 @@
+package boj10430;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        //(A+B)%C
+        System.out.println((A + B) % C);
+        // ((A%C) + (B%C))%C
+        System.out.println(((A % C) + (B % C)) % C);
+        //(A×B)%C
+        System.out.println((A * B) % C);
+        //((A%C) × (B%C))%C
+        System.out.println(((A % C) * (B % C)) % C);
+
+        br.close();
+    }
+}
