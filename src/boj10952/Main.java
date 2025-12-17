c240a0493bf268e4a7a6476540a8a0aee16aac54
이상원 <sangwon020214@gmail.com>
2025-12-17 15:36:58 +0900

    Solved: BOJ 10952 A+B - 5

--- src/boj10952/Main.java
+++ src/boj10952/Main.java
@@ -11,8 +11,11 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     while (true) {
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String line = br.readLine();
+        // 입력이 없거나 비정상적인 경우 종료 (안전장치)
+        if (line == null) break;
 
+        StringTokenizer st = new StringTokenizer(line);
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
