560e90830ec9a527b8fcb31fa48e4b9938ef6e2c
이상원 <sangwon020214@gmail.com>
2025-12-18 10:20:50 +0900

    Solved: BOJ 11720 숫자의 합

--- src/boj11720/Main.java
+++ src/boj11720/Main.java
@@ -1,19 +1,30 @@
 package boj11720;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-        String numbers = sc.next();
-        int res = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        String num = br.readLine();
+
+        int sum = 0;
+
 
         for (int i = 0; i < n; i++) {
-            res += numbers.charAt(i) - '0';
+            // 문자열에서 한 글자씩 꺼냄
+            char c = num.charAt(i);
+            // 문자를 정수로 변환
+            int number = Integer.parseInt(c + "");
+
+            sum += number;
+
         }
 
-        System.out.println(res);
-        sc.close();
+        System.out.println( sum);
     }
 }
