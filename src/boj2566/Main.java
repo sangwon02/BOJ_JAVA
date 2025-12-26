3908d52ef422f1b5f1d1a1ae3bada11238d5c6db
이상원 <sangwon020214@gmail.com>
2025-12-26 20:45:23 +0900

    Solved: BOJ 2566 최댓값

--- src/boj2566/Main.java
+++ src/boj2566/Main.java
@@ -1,5 +1,6 @@
 package boj2566;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,27 +10,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 최댓값을 저장할 변수
-        int max = 0;
-        // 최댓값의 위치를 저장할 변수
-        int maxRow = 1;
-        int maxCol = 1;
+        int max = 0; // 최댓값, -1로 시작
+        int row = 0;  // 최댓값의 행
+        int col = 0;  // 최댓값의 열
 
-        for (int i = 1; i <= 9; i++) { // 1행부터 9행
+        // 9행 반복
+        for (int i = 1; i <= 9; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= 9; j++) { // 1열부터 9열
+
+            // 9열 반복
+            for (int j = 1; j <= 9; j++) {
+
                 int num = Integer.parseInt(st.nextToken());
 
+                // 현재 값이 최댓값보다 크면 갱신
                 if (num > max) {
+
                     max = num;
-                    maxRow = i;
-                    maxCol = j;
+                    row = i; // 행 저장
+                    col = j; // 열 저장
                 }
             }
         }
 
         System.out.println(max);
-        System.out.println(maxRow + " " + maxCol);
+
+        System.out.println(row + " " + col);
 
         br.close();
     }
