983905668c2b2254ed37f7cc37cfd3cf34e3a2ab
이상원 <sangwon020214@gmail.com>
2026-01-04 21:37:37 +0900

    Solved: BOJ 2798 블랙잭

--- src/boj2798/Main.java
+++ src/boj2798/Main.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,22 +22,20 @@ public class Main {
             cards[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 답을 담을 변수
         int res = 0;
 
-        // 3중 for문으로 3개의 카드를 뽑는 모든 조합을 만든다
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
                     int currentSum = cards[i] + cards[j] + cards[k];
 
-                    // 현재 합이 m을 넘지 않으면서, 이전에 찾은 합보다 더 크다면
                     if (currentSum <= m && currentSum > res) {
                         res = currentSum;
                     }
                 }
             }
         }
+
         System.out.println(res);
         br.close();
 
