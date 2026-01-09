db482be8a2e2a4e6066e40ed3ec5c8214821823f
이상원 <sangwon020214@gmail.com>
2026-01-09 23:38:35 +0900

    Solved: 스터디

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    // 자바 프로그램은 이 main 메서드에서 실행을 시작
-    public static void main(String[] args) {
-        // main 메서드 안에서 Hello, World! 출력
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n =Integer.parseInt(br.readLine());
+
+        int num = 0;
+
+        while (n > num) {
+            num += 1;
+            int num2 = num;
+            int sum = 0;
+            while (num2 != 0) {
+                sum += num2%10;
+                num2 = num2/10;
+            }
+            if ((sum+num)==n) {
+                System.out.println(num);
+                br.close();
+                System.exit(0);
+
+            }
+        }
+
+        System.out.println(0);
+        br.close();
+
     }
 }
