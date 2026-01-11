ce42f1e27ea9b326580724dad13628f62e15ae0d
이상원 <sangwon020214@gmail.com>
2026-01-11 22:02:19 +0900

    git commit -m "Solved: BOJ 11050 이항 계수 1"

---
+++ src/boj11050/Main.java
@@ -0,0 +1,32 @@
+package boj11050;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int top = factorial(n);
+        int bottom = factorial(k)*factorial(n-k);
+
+        System.out.println(top / bottom);
+
+        br.close();
+
+    }
+}
