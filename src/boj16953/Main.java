db575fa7606a1af7c74f40b66d56d665d947339b
이상원 <sangwon020214@gmail.com>
2026-01-20 16:22:32 +0900

    Solved: BOJ 16953 A -> B

--- src/boj16953/Main.java
+++ src/boj16953/Main.java
@@ -6,14 +6,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public  static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        // b를 a로 바꾸자
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int cnt = 1;
 
+        while (b != a) {
 
+            // a보다 작아지면 실패
+            if (b < a) {
+                cnt = -1;
+                break;
+            }
+
+            // 끝자리가 1인 경우 1을 뗀다
+            if (b % 10 == 1) {
+                b -= 1;
+                b /= 10;
+            } else if (b % 2 == 0) { // 짝수면 2로 나눔
+                b /= 2;
+            } else {
+                cnt = -1;
+                break;
+            }
+
+            cnt++;
+        }
+
+        System.out.println(cnt);
 
     }
 }
