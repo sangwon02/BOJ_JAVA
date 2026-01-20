7a3f3a7012dcbebd99c1a7f43b402875fd5a3d94
이상원 <sangwon020214@gmail.com>
2026-01-20 15:53:01 +0900

    Solved: BOJ 16953 N과 M (1)

--- src/boj15649/Main.java
+++ src/boj15649/Main.java
@@ -6,48 +6,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static int n;
-    public static int m;
-    public static int[] arr; // 수열을 저장할 배열
-    public static boolean[] visited; // 방문 여부를 체크할 배열
+    public static int[] output;
+    public static int n, m;
+    public static boolean[] visited;
     public static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arr = new int[m];
-        visited = new boolean[n + 1];
-
-        // 백트래킹 함수 시작 (깊이 0부터)
-        backtrack(0);
-
-        System.out.println(sb); // 최종 결과 출력
-        br.close();
-    }
-
-    public static void backtrack(int depth) {
-        // 종료 조건: 수열이 M개가 되면 출력하고 종료
-        if (depth == m) {
-            for (int val : arr) {
+    public static void perm(int cnt) {
+        // [종료 조건] m개를 모두 뽑았으면 출력하고 종료
+        if (cnt == m) {
+            for (int val : output) {
                 sb.append(val).append(' ');
             }
             sb.append('\n');
             return;
         }
 
-        //1부터 N까지의 수를 탐색
         for (int i = 1; i <= n; i++) {
-            // 아직 방문하지 않은 숫자라면
-            if (!visited[i]) {
-                visited[i] = true;  //선택 (사용했다고 표시)
-                arr[depth] = i;  // 현재 깊이에 해당 숫자를 저장
-                backtrack(depth + 1); //탐색 (더 깊이 들어가기)
-                visited[i] = false; //선택 취소 (원상 복구)
-            }
+            // 이미 사용한 숫자라면 건너뜀
+            if (visited[i]) continue;
+
+            // [선택] i번째 숫자를 선택
+            output[cnt] = i;      // 결과 배열에 저장
+            visited[i] = true;    // 사용했다고 표시
+
+            // [탐색] 다음 숫자를 뽑으러 감 (재귀 호출)
+            perm(cnt + 1);
+
+            // [선택 취소] 탐색이 끝나고 돌아오면, 다음 경우를 위해 사용 표시 해제
+            visited[i] = false;
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        output = new int[m];
+        visited = new boolean[n + 1];
+
+        perm(0);
+
+        System.out.print(sb);
     }
 }
