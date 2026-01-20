5466c304ffa790564e56b551f5fa7dc8cfe8d1aa
이상원 <sangwon020214@gmail.com>
2026-01-20 15:18:03 +0900

    Solved: BOJ 10816 숫자 카드 2

--- src/boj10816/Main.java
+++ src/boj10816/Main.java
@@ -1,43 +1,43 @@
 package boj10816;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public  static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 해시맵 생성
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // 상근이가 가진 카드 N개 입력받음
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //맵에 카드 개수를 세어서 저장
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> nCard = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            int card = Integer.parseInt(st.nextToken());
+            nCard.put(card, nCard.getOrDefault(card, 0) + 1);
         }
 
-        //확인할 카드 M개 입력받기
-        int M = Integer.parseInt(br.readLine());
+
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
-        // M개의 카드를 순회하며 맵에서 개수 찾기
-        for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            bw.write(map.getOrDefault(target, 0) + " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= m; i++) {
+            int card = Integer.parseInt(st.nextToken());
+            if (nCard.containsKey(card)) {
+                sb.append(nCard.get(card)).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
         }
 
+        System.out.println(sb);
+
         br.close();
-        bw.flush();
-        bw.close();
+
     }
 }
