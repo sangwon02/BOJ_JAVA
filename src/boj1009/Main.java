2a48ec6afae5f6ccab230661798d7a406b3c7c84
이상원 <sangwon020214@gmail.com>
2026-01-24 23:16:09 +0900

    Solved: BOJ 1009 분산처리

---
+++ src/boj1009/Main.java
@@ -0,0 +1,83 @@
+package boj1009;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a1 = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int a = a1%10;
+            if (a==1) {
+                System.out.println(1);
+            } else if (a==2) {
+                if (b%4 == 1) {
+                    System.out.println(2);
+                } else if (b%4 == 2) {
+                    System.out.println(4);
+                } else if (b%4 == 3) {
+                    System.out.println(8);
+                } else if (b%4 == 0) {
+                    System.out.println(6);
+                }
+            } else if (a==3) {
+                if (b%4 == 1) {
+                    System.out.println(3);
+                } else if (b%4 == 2) {
+                    System.out.println(9);
+                } else if (b%4 == 3) {
+                    System.out.println(7);
+                } else if (b%4 == 0) {
+                    System.out.println(1);
+                }
+            } else if (a==4) {
+                if (b%2 == 1) {
+                    System.out.println(4);
+                } else if (b%2 == 0) {
+                    System.out.println(6);
+                }
+            } else if (a==5) {
+                System.out.println(5);
+            } else if (a==6) {
+                System.out.println(6);
+            } else if (a==7) {
+                if (b%4 == 1) {
+                    System.out.println(7);
+                } else if (b%4 == 2) {
+                    System.out.println(9);
+                } else if (b%4 == 3) {
+                    System.out.println(3);
+                } else if (b%4 == 0) {
+                    System.out.println(1);
+                }
+            } else if (a==8) {
+                if (b%4 == 1) {
+                    System.out.println(8);
+                } else if (b%4 == 2) {
+                    System.out.println(4);
+                } else if (b%4 == 3) {
+                    System.out.println(2);
+                } else if (b%4 == 0) {
+                    System.out.println(6);
+                }
+            } else if (a==9) {
+                if (b%2 == 1) {
+                    System.out.println(9);
+                } else if (b%2 == 0) {
+                    System.out.println(1);
+                }
+            } else if (a==0) {
+                System.out.println(10);
+            }
+        }
+        br.close();
+    }
+}
