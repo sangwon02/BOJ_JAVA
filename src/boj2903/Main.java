7eca3e004a6679c7c502cc8bcc0aea7de3f6d4d5
이상원 <sangwon020214@gmail.com>
2026-02-05 16:26:53 +0900

    Solved: BOJ 2903 중앙 이동 알고리즘

---
+++ src/boj2903/Main.java
@@ -0,0 +1,18 @@
+package boj2903;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public  static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int res = 2;
+        for (int i = 1; i <= n; i++) {
+            res = (res-1) + res;
+        }
+        System.out.println(res*res);
+    }
+}
