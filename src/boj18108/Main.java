2f4383603670d65fdf0a76f11afc5c977fad7f33
이상원 <sangwon020214@gmail.com>
2026-02-17 22:50:41 +0900

    Solved: BOJ 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/boj18108/Main.java
@@ -0,0 +1,17 @@
+package boj18108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(y - 543);
+
+        br.close();
+    }
+}
