63a7040623a103fe531158a509a017090278f82e
이상원 <sangwon020214@gmail.com>
2026-03-02 21:36:17 +0900

    Solved: BOJ 9086 문자열

---
+++ src/boj9086/Main.java
@@ -0,0 +1,18 @@
+package boj9086;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for  (int i = 0; i < n; i++) {
+            String st =  br.readLine();
+            StringBuilder sb = new StringBuilder();
+            sb.append(st.charAt(0)).append(st.charAt(st.length()-1));
+            System.out.println(sb);
+        }
+    }
+}
